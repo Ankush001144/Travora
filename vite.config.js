51e01c629321ee6bdd43a671afbcd355a4ac3fa0
Ankush001144 <ankushmishra431@gmail.com>
2026-05-10 15:54:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/YOUR-REPOSITORY-NAME/',
+  base: '/Travora/',
   plugins: [react(), tailwindcss()],
 })
